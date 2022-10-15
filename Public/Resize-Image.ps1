88be470be623159b62fd7e9e59621adb7169cf42
Fabio Hodel <fabiohodel1@gmail.com>
2022-10-15 14:57:38 +0200

    Added Resize-Image

--- Public/Resize-Image.ps1
+++ Public/Resize-Image.ps1
@@ -1,1 +1,47 @@
-# To do...
+<#
+.SYNOPSIS
+    Resize-Image
+.DESCRIPTION
+    Resize an image and save it to the specified OutFile path.
+.EXAMPLE
+.INPUTS
+.OUTPUTS
+.NOTES
+    Author: Fxbyy
+.FUNCTIONALITY
+    Resize an image and save it to the specified OutFile path.
+#>
+function Resize-Image {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $InFile,
+
+        [Parameter(Mandatory = $true)]
+        [string] $OutFile,
+
+        [Parameter(Mandatory = $true)]
+        [int] $Width,
+
+        [Parameter(Mandatory = $true)]
+        [int] $Height
+    )
+    
+    begin {
+        [void][System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+    }
+    
+    process {
+        $img = [System.Drawing.Image]::FromFile((Get-Item $InFile))
+
+        $resizedImage = New-Object System.Drawing.Bitmap($Width, $Height)
+
+        $graphics = [System.Drawing.Graphics]::FromImage($resizedImage)
+        $graphics.DrawImage($img, 0, 0, $Width, $Height)
+        $graphics.Dispose()
+    }
+    
+    end {
+        $resizedImage.Save($OutFile)
+    }
+}
--- test.ps1
+++ test.ps1
@@ -1,12 +1,12 @@
 $Params = @{ 
-    "Path" 				= 'D:\02_Coding\PowerShell\PSImageToolbox\PSImageToolbox\PSImageToolbox.psd1' 
-    "Author" 			= 'Fxbyy' 
-    "RootModule" 			= 'PSImageToolbox.psm1' 
-    "CompatiblePSEditions" 		= @('Desktop','Core') 
-    "FunctionsToExport" 		= @('Convert-ColorHexToRGB','Convert-ColorToImage','Convert-ImageToAscii','Resize-Image') 
-    "CmdletsToExport" 		= @() 
-    "VariablesToExport" 		= '' 
-    "AliasesToExport" 		= @() 
-    "Description" = 'A few PowerShell Cmdlets to work (or play) with images, colors etc.' 
+    "Path"                 = 'D:\02_Coding\PowerShell\PSImageToolbox\PSImageToolbox\PSImageToolbox.psd1' 
+    "Author"               = 'Fxbyy' 
+    "RootModule"           = 'PSImageToolbox.psm1' 
+    "CompatiblePSEditions" = @('Desktop', 'Core') 
+    "FunctionsToExport"    = @('Convert-ColorHexToRGB', 'Convert-ColorToImage', 'Convert-ImageToAscii', 'Resize-Image') 
+    "CmdletsToExport"      = @() 
+    "VariablesToExport"    = '' 
+    "AliasesToExport"      = @() 
+    "Description"          = 'A few PowerShell Cmdlets to work (or play) with images, colors etc.' 
 } 
 New-ModuleManifest @Params
